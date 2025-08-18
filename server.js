24cad34cbf60ed94372078f416e51e4304a9292f
prasanna0218 <prasannampmrasanna@gmail.com>
2025-08-18 10:47:21 +0530

    3rd api added

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.get("/test2",(req,res)=>{
     return res.send("Hi I'am from the new test API 2 ....!");
 })
 
+app.get("/test3",(req,res)=>{
+    return res.send("3rd API ....");
+})
+
 app.listen(3000,()=>{
     console.log("Server is listening on port 3000 !");
 })
@@ -22,3 +26,8 @@ app.listen(3000,()=>{
 
 // codebuild-my-nodejs-be-ecs-codebuild-service-role
 //:aws:elasticloadbalancing:us-east-1:076542168620:targetgroup/my-nodejsbe-tg/d11c683cc4452cd6
+
+//arn:aws:ecs:us-east-1:076542168620:task/my-nodejs-be-ecs/2eb425501c8a4e1d86667a91bbc3c796
+// arn:aws:ecs:us-east-1:076542168620:task/my-nodejs-be-ecs/2eb425501c8a4e1d86667a91bbc3c796
+//arn:aws:ecs:us-east-1:076542168620:task/my-nodejs-be-ecs/b6b269c0884e41ce9aabaebdb8f0d232
+// arn:aws:ecs:us-east-1:076542168620:task/my-nodejs-be-ecs/b6b269c0884e41ce9aabaebdb8f0d232
